a1d4ccb67af78f4e5e37fdd654805d021a6ed97f
Barun <bhattacharjeebarun25@gmail.com>
2020-10-22 18:01:03 +0600

    Sign up bug fixed

--- users/models.py
+++ users/models.py
@@ -5,6 +5,7 @@ import uuid
 
 
 class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
     country = models.CharField(max_length=100)
     city_or_district = models.CharField(max_length=100)
     uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
